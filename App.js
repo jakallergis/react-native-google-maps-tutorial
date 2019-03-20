a0ba3e2140ff2677f458d2bfe3f4d32b251244c1
John A. Kallergis <j.a.kallergis@gmail.com>
2019-03-20 19:49:11 +0200

    feat: added boilerplate layout

--- App.js
+++ App.js
@@ -7,12 +7,65 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, { Component }         from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { Component }                       from 'react';
+import { StyleSheet, View, Button, SafeAreaView } from 'react-native';
+import MapView, { PROVIDER_GOOGLE }               from 'react-native-maps';
+
+/** Models / Types */
+import type { Region }                            from 'react-native-maps';
 
 type Props = {};
-export default class App extends Component<Props> {
+type State = { region: ?Region, }
+export default class App extends Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = { region: null };
+  }
+
+  /** Button Handlers */
+
+  _showFacebook = (): void => {};
+  _showGoogle = (): void => {};
+  _showEiffelTower = (): void => {};
+
+  /** Renderers */
+
   render() {
-    return <MapView provider={ PROVIDER_GOOGLE } style={ { flex: 1 } } />
+    return (
+      <View style={ styles.container }>
+        <MapView
+          provider={ PROVIDER_GOOGLE }
+          region={ this.state.region }
+          style={ styles.mapViewContainer } />
+        <View style={ styles.buttonsContainer }>
+          <Button
+            title={ 'Facebook' }
+            onPress={ this._showFacebook } />
+          <Button
+            title={ 'Google Plex' }
+            onPress={ this._showGoogle } />
+          <Button
+            title={ 'Eiffel Tower' }
+            onPress={ this._showEiffelTower } />
+        </View>
+        <SafeAreaView />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  mapViewContainer: {
+    flex: 1
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingVertical: 16
+  }
+});
