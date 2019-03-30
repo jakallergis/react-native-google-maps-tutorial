6d26e94f3e5c1509e9f110016a7552d541ecf0a0
John A. Kallergis <j.a.kallergis@gmail.com>
2019-03-31 01:27:45 +0200

    style: code cleanup

--- src/utilities/poi.js
+++ src/utilities/poi.js
@@ -3,30 +3,70 @@
  * @flow
  */
 
-export const APPLE = {
+export const APPLE_COORDINATES = {
   latitude: 37.446380,
-  longitude: -122.160751,
+  longitude: -122.160751
+};
+
+export const FACEBOOK_COORDINATES = {
+  latitude: 37.485178,
+  longitude: -122.147135
+};
+
+export const GOOGLE_PLEX_COORDINATES = {
+  latitude: 37.422264,
+  longitude: -122.084036
+};
+
+export const EIFFEL_TOWER_COORDINATES = {
+  latitude: 48.858570,
+  longitude: 2.294493
+};
+
+export const APPLE = {
+  ...APPLE_COORDINATES,
   latitudeDelta: 0.02,
   longitudeDelta: 0.02
+};
+
+export const APPLE_OUT = {
+  ...APPLE_COORDINATES,
+  latitudeDelta: 0.2,
+  longitudeDelta: 0.2
 };
 
 export const FACEBOOK = {
-  latitude: 37.485178,
-  longitude: -122.147135,
+  ...FACEBOOK_COORDINATES,
   latitudeDelta: 0.02,
   longitudeDelta: 0.02
+};
+
+export const FACEBOOK_OUT = {
+  ...FACEBOOK_COORDINATES,
+  latitudeDelta: 0.2,
+  longitudeDelta: 0.2
 };
 
 export const GOOGLE_PLEX = {
-  latitude: 37.422264,
-  longitude: -122.084036,
+  ...GOOGLE_PLEX_COORDINATES,
   latitudeDelta: 0.02,
   longitudeDelta: 0.02
 };
 
+export const GOOGLE_PLEX_OUT = {
+  ...GOOGLE_PLEX_COORDINATES,
+  latitudeDelta: 0.2,
+  longitudeDelta: 0.2
+};
+
 export const EIFFEL_TOWER = {
-  latitude: 48.858570,
-  longitude: 2.294493,
-  latitudeDelta: 0.02,
-  longitudeDelta: 0.02
+  ...EIFFEL_TOWER_COORDINATES,
+  latitudeDelta: 0.1,
+  longitudeDelta: 0.1
+};
+
+export const EIFFEL_TOWER_OUT = {
+  ...EIFFEL_TOWER_COORDINATES,
+  latitudeDelta: 0.2,
+  longitudeDelta: 0.2
 };
